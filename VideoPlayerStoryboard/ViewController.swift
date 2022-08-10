e32300ca3bda166ab25fbd6b6ab438da8f7cca89
Jai Dhorajia <jai.dhorajia@ibm.com>
2022-08-10 09:50:56 -0300

    Added UI components and initial viewcontroller setup
    Added all the UI components and organised their outlets and initial setup of the viewcontroller

--- VideoPlayerStoryboard/ViewController.swift
+++ VideoPlayerStoryboard/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    // MARK: - Variables
+    
+    // MARK: - View Delegates
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Helper Methods
+    
+    func setupUI() {
+        
+    }
 
+    
+    // MARK: - IBActions
+    
+    @IBAction func playButton_pressed(_ sender: Any) {
+    }
+    
+    @IBAction func previousButton_pressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextButton_pressed(_ sender: Any) {
+    }
 }
 
